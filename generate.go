de67aa7b1d92cb934a0146dad27659c41fdb0ef6
Alexander Backlund <alexander.backlund@sudosweden.com>
2025-09-12 14:21:40 +0200

    Update controller-gen to be managed as tool dependency

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd object rbac:roleName=manager-role paths="./..."
+//go:generate go tool controller-gen crd object rbac:roleName=manager-role paths="./..."
